e041551e27232dfebeb0910a0da7818ca98253b0
Zachary Youngdale <zyoungdale@gmail.com>
2022-11-03 11:35:38 -0400

    commit tree 52c0360808c18ef4bf7bf9381874503764f3950c

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,42 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id] 
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
+
+  def show 
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    new_item = user.items.create(item_params)
+    render json: new_item, status: :created
+  end
+
+
+
+
+
+
+
+private
+
+def item_params 
+  params.permit(:name, :description, :price)
+end
+
+def render_not_found_response
+  render json: {errors: "Item not found"}, status: :not_found
+end
+
 
 end
